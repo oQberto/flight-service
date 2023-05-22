286972f3aa8eba63c0f68012b1574131faed6ea2
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-05-22 20:37:25 +0300

    feat: add FlightServlet.java

---
+++ src/main/java/com/example/flightservice/servlet/FlightServlet.java
@@ -0,0 +1,23 @@
+package com.example.flightservice.servlet;
+
+import com.example.flightservice.service.FlightService;
+import com.example.flightservice.util.JspHelper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/flights")
+public class FlightServlet extends HttpServlet {
+    private final FlightService flightService = FlightService.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("flights", flightService.findAll());
+        req.getRequestDispatcher(JspHelper.get("flights"))
+                .forward(req, resp);
+    }
+}
