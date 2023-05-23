4da64fad899a80f42871d49c866d5e4274b780b0
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-05-24 00:18:34 +0300

    refactor: change TicketServlet.java

--- src/main/java/com/example/flightservice/servlet/TicketServlet.java
+++ src/main/java/com/example/flightservice/servlet/TicketServlet.java
@@ -1,5 +1,6 @@
 package com.example.flightservice.servlet;
 
+import com.example.flightservice.service.TicketService;
 import com.example.flightservice.util.JspHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,9 +12,12 @@ import java.io.IOException;
 
 @WebServlet("/tickets")
 public class TicketServlet extends HttpServlet {
+    private final TicketService ticketService = TicketService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Long flightId = Long.valueOf(req.getParameter("flightId"));
+        req.setAttribute("tickets", ticketService.findByFlightId(flightId));
         req.getRequestDispatcher(JspHelper.get("ticket"))
                 .forward(req, resp);
     }
